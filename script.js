3797f2938d7b9bc6eb2c9ed7b7cdd57e9a49dee3
NoelaniFish <noelanifishman@gmail.com>
2024-11-13 07:35:01 -0500

    Update script.js

--- script.js
+++ script.js
@@ -27,7 +27,6 @@ function initSpeechRecognition() {
     recognition.lang = 'en-US';
 
     recognition.onstart = () => {
-        console.log("Listening...");
         statusText.textContent = "Listening...";
         statusText.classList.add('listening');
         statementStartTime = new Date().getTime();
@@ -39,19 +38,15 @@ function initSpeechRecognition() {
         const confidence = event.results[0][0].confidence;
         const transcriptLength = transcript.trim().length;
 
-        console.log("Transcript:", transcript);
-        console.log("Confidence:", confidence);
-
-        if (confidence > 0.7 && transcriptLength > 10) {
+        if (confidence > 0.6 && transcriptLength > 5) {
             const duration = (statementEndTime - statementStartTime) / 1000;
             categorizeAndRespond(transcript, duration);
         } else {
-            console.log("Ignored due to low confidence or short input.");
+            playAudioForDuration(audios.greeting, 2);
         }
     };
 
     recognition.onspeechend = () => {
-        console.log("Speech ended");
         recognition.stop();
         statusText.textContent = "Not Listening";
         statusText.classList.remove('listening');
@@ -66,31 +61,39 @@ function initSpeechRecognition() {
     };
 }
 
-// Categorize speech and respond
+// Updated categorization with enhanced keywords
 function categorizeAndRespond(text, duration) {
     let audio;
 
-    if (/\b(hi|hello|hey|greetings|goodbye|farewell|how’s it going)\b/.test(text)) {
-        audio = audios.greeting;
-    } else if (/\b(calm|love|family|support|peace|gentle|kind)\b/.test(text)) {
+    // **Motherly / Nurturing / Everyday Man Speak**
+    if (/\b(calm|love|support|peace|gentle|sweet|comfort|home|supportive|hug|take it easy|you’ve got this|rest up|let’s catch up|no worries|it’s all good|I’m here for you|take care|don’t stress it|hang in there|you’re doing great|you’ve got my support|just relax|you’re safe|I’m proud of you|focus on yourself|just breathe)\b/.test(text)) {
         audio = audios.motherly;
-    } else if (/\b(tear|beatdown|crush|burn|threat|fuck|kill)\b/.test(text)) {
+
+    // **Aggressive / Angry / Everyday Man Speak**
+    } else if (/\b(tear|destroy|rage|fight|cancel culture|toxic | piss|pissed off|fed up|cut it out|I’m over it|mind your business|get lost|stop it now|you crossed the line|that’s enough|you’ve gone too far|stay out of my way|enough is enough|get a grip|back off|I’ve had it with you|don’t mess with me|I’m done with this)\b/.test(text)) {
         audio = audios.aggressive;
-    } else if (/\b(me|myself|blame|judge|I’m trying|stop)\b/.test(text)) {
+
+    // **Defensive / Insecure / Everyday Man Speak**
+    } else if (/\b(me|myself|stop|defend|I’m scared|that’s not fair|you don’t understand|why me?|I’m trying my best|please don’t judge me|I didn’t mean it|I don’t know what else to do|why is this always happening to me?|I’m not wrong|I don’t belong here|I feel lost|why are you blaming me?|I’m doing my best)\b/.test(text)) {
         audio = audios.defensive;
-    } else if (/\b(eyes|smile|love|flirt|sexy|vibe|dance)\b/.test(text)) {
+
+    // **Flirtatious / Playful / Everyday Man Speak**
+    } else if (/\b(eyes|smile|love|cute|fun|flirt|crush|bae|you look great|nice to see you|we should hang out|let’s grab a drink|you’ve got great vibes|I can’t stop thinking about you|I love your smile|you’re fun to be around|let’s do this again|you light up the room|you’re really cool|I’ve got a crush on you|you’re so easy to talk to)\b/.test(text)) {
         audio = audios.flirtatious;
-    } else if (/\b(danger|run|alert|stay away|get out)\b/.test(text)) {
+
+    // **Potential Danger / Warning / Everyday Man Speak**
+    } else if (/\b(danger|alert|run|red flag|be careful|get out|watch out|stay safe|this isn’t good|take cover|it’s not safe|hold up|heads up|stay alert|back away|don’t go there|that’s risky|get away|this could get bad|it’s a trap|look out)\b/.test(text)) {
         audio = audios.danger;
-    } else if (/\b(freaking|shaking|scared|panic|terrified|nightmare)\b/.test(text)) {
+
+    // **Terrified / Fear / Everyday Man Speak**
+    } else if (/\b(freaking out|shaking|scared|panic|help me|dread|I’m losing it|terrified|I’m so scared|I’m not okay|I can’t breathe|I’m freaking out|I feel paralyzed|I’m about to lose it|this is too much|I’m having a meltdown|my heart is racing|I’m not sure what to do|I feel trapped|I’m panicking)\b/.test(text)) {
         audio = audios.terrified;
-    } else if (/\b(mine|territory|claim|protect|right)\b/.test(text)) {
-        audio = audios.territorial;
+
+    } else {
+        audio = audios.greeting;
     }
 
-    if (audio) {
-        playAudioForDuration(audio, duration);
-    }
+    playAudioForDuration(audio, duration);
 }
 
 // Play audio for specified duration
